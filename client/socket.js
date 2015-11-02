cc031ad9ed711261eab74090af29f2d78c470574
Frederic Gascon <frederic.gascon@gmail.com>
2015-11-01 21:43:41 -0500

    socket uses baseUrl

--- client/socket.js
+++ client/socket.js
@@ -1,3 +1,8 @@
 import SockSubIO from 'socksub.io';
+import url from 'url';
 
-module.exports = new SockSubIO();
+const parsedUrl = url.parse(BASE_URL);
+
+module.exports = new SockSubIO(parsedUrl.protocol+'//'+parsedUrl.host, {
+    path: parsedUrl.pathname + 'socket.io'
+});
