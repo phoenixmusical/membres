550f81757a9b180054fca912b5d36e0789dff354
Frederic Gascon <frederic.gascon@gmail.com>
2015-11-14 17:27:05 -0500

    home page show only content for subscribed comities

--- client/pages/HomePage.js
+++ client/pages/HomePage.js
@@ -14,6 +14,15 @@ export default class HomePage extends Page {
     getPageName () {
         return 'home';
     }
+    receiveData (data) {
+        if (data.events) {
+            data.events.forEach(function (event) {
+                event.start = new Date(event.start);
+                event.end = new Date(event.end);
+            });
+        }
+        this.setState(data);
+    }
     render () {
         return (
             <div className="row">
